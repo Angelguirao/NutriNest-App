24033ac80405cc0f6046b852f66ac46ea517b0ad
Alcaraz <jaime.mediano@gmail.com>
2023-07-14 16:44:34 +0200

    Ingredients page working perfectly

---
+++ config/cloudinary.config.js
@@ -0,0 +1,26 @@
+// config/cloudinary.config.js
+ 
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const multer = require('multer');
+ 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET
+});
+ 
+const storage = new CloudinaryStorage({
+  // cloudinary: cloudinary,
+  cloudinary,
+  folder: 'ironHack-nutrition-project',
+  allowed_formats: ['jpg', 'png'],
+  filename: function (req, res, cb) {
+    cb(null, res.originalname);
+  }
+     // The name of the folder in cloudinary
+    // resource_type: 'raw' => this is in case you want to upload other type of files, not just images
+  
+});
+ 
+module.exports = multer({ storage });
